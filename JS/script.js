8f503514dc6924086b2e7baa5be60934f19410f9
kimstoc01 <kimstocker2020@gmail.com>
2021-07-27 17:49:43 -0400

    trying js and media query updates

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
-document.getElementById("myBtn").addEventListener("click", function() {
-    alert("Hello World!");
-  });
+const submitButton = document.getElementById("submit");
+let name = document.getElementsById("name");
+
+submitButton.addEventListener("click", sayThanks)
+
+function sayThanks() {
+    alert("Thank you ${name} for your request! We will get back with you as soon as possible to bring your idea to life!");
+}
+
+function emailIsValid (email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+      }
