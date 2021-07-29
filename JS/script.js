7e0c119936587c3f7b241b8c94498406ab8efa43
kimstoc01 <kimstocker2020@gmail.com>
2021-07-29 08:39:37 -0400

    css and javascript to-dos

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,48 @@
-const submitButton = document.getElementById("submit");
-let name = document.getElementsById("name");
 
-submitButton.addEventListener("click", sayThanks)
 
-function sayThanks() {
-    alert("Thank you ${name} for your request! We will get back with you as soon as possible to bring your idea to life!");
+//upon submit validating and showing confirmation mssg
+function contactValidation() {
+    
+    //validate email
+    const email = document.getElementById("email");
+    const emailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+   
+    //validate name is required
+    //to do: add name requirement
+    const name = document.getElementById("name");
+
+    let isValid= false;
+    if(emailValidator) {
+        isValid= true;
+    }
+
+    //todo: add check for name require, if true, set isValid to true
+
+    //if form passes validation, show confirmation message, hide form
+    if(isValid) {
+        confirmationMessage();
+        event.preventDefault();  //this stop form submission from redirect back to itself
+        return false; //returns false so the page stops any futher execution
+    }
 }
 
-function emailIsValid (email) {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-      }
+
+//this function is hidding the form upon sucessful validation and showing the confirmation message
+function confirmationMessage() {
+    var x = document.getElementById("contact_form");
+    //getting the hidden confirmation control
+    const confirmationMessage = document.getElementById("confirmationMessage");
+    
+    
+    if (x.style.display === "none") {
+        //this is showing the form
+      x.style.display = "block";
+      //hidding the confirmation control
+      confirmationMessage.style.display = "none";
+    } else {
+        //this hides the form
+      x.style.display = "none";
+      //this shows the confirmation control
+      confirmationMessage.style.display = "block";
+    }
+  }
